8b6400a0524a1c2b433ec18b35fcf6aab23eb832
Aex <spryti@qq.com>
2021-04-07 16:25:36 +0800

    新增AllinPay类型的导出

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { AllinPay } from './typings'
 export { AllinPayBin } from './libs/AllinPayBin'
 export { AllinPayOrderService } from './services/AllinPayOrderService'
 export { AllinPayMemberService } from './services/AllinPayMemberService'
